9bcf22749115c7ac2c4b265aa89d2cc06418a140
Brian Jacobs <brianjcbs23@gmail.com>
2017-04-24 19:07:58 -0400

    Added psudocode comments in the main function

--- source/main.c
+++ source/main.c
@@ -34,7 +34,20 @@ int main()
     gpio[GPFSEL2] |= (1 << 12);
 
     while(1){
-        
+        // generate the mastermind codeword
+            // char [4] codeword;
+            // void mastermind(codeword);
+        // prompt the user for their first guess
+            // while{print_string("Take a Guess\r\n>> ")
+            // char [5] guess
+            // get_string(guess, 5);
+        // compare the user's guess against the mastermind's codeword
+            // if guess[i] == codeword[i]...; else if guess[i] == codeword[0..3]...;
+        // illuminate or blink the required LED's
+            // if correct solid(); else if incorrect, but is somewhere else blink(); else nothing;
+        // prompt the user for their next guess
+            // print_string("\r\n")}
+        // repeat 10 times or until the user guesses the correct codeword
     }
     
     return 0;
