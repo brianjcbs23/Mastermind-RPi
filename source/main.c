9c60e70de92cd73a1116f6af013882ae99f208a3
Paul Hulbert <pxh8242@rit.edu>
2017-04-28 10:46:42 -0400

    Added random and modulo functions, and generating code

--- source/main.c
+++ source/main.c
@@ -27,6 +27,38 @@ void blink_code(uint32_t err)
     timer_delay_ms(4500);
 }
 
+
+//Current state of the random
+uint32_t state = 100;
+
+
+//Modulo function
+uint32_t mod(uint32_t dividend, uint32_t divisor) {
+    while (dividend > divisor) {
+        dividend -= divisor;
+    }
+
+    return dividend;
+}
+
+
+//Generates a random integer with a minimum value of min and a maximum value of max. 
+//Range is inclusive
+uint32_t random(uint32_t min, uint32_t max) {
+    state = state xor (state << 13);
+    state = state xor (state >> 17);
+    state = state xor (state << 5);
+
+    uint32_t tmp = mod(state, max);
+    while (tmp < min) {
+        tmp += min;
+        tmp = mod(tmp, max);
+    }
+
+    return tmp;
+}
+
+
 int main()
 {
     init_uart();
@@ -35,18 +67,39 @@ int main()
 
     while(1){
         // generate the mastermind codeword
-            // char [4] codeword;
-            // void mastermind(codeword);
+        // char [4] codeword;
+        char codeword[4] = {0,0,0,0};
+        char options[6] = {'1','2','3','4','5','6'};
+
+        //Loop through and generate random codeword
+        while (codeword[0] == 0 || codeword[1] == 0 || codeword[2] == 0 || codeword[3] == 0) {
+            char newLetter = options[random(0, 5)];
+
+            for (int i = 0; i < 4; i++) {
+                if (codeword[i] == 0) {
+                    codeword[i] = newLetter;
+                    break;
+                }
+                if (codeword[i] == newLetter) {
+                    break;
+                }
+            }
+        }
+
+
+
+
+        // void mastermind(codeword);
         // prompt the user for their first guess
-            // while{print_string("Take a Guess\r\n>> ")
-            // char [5] guess
-            // get_string(guess, 5);
+        // while{print_string("Take a Guess\r\n>> ")
+        // char [5] guess
+        // get_string(guess, 5);
         // compare the user's guess against the mastermind's codeword
-            // if guess[i] == codeword[i]...; else if guess[i] == codeword[0..3]...;
+        // if guess[i] == codeword[i]...; else if guess[i] == codeword[0..3]...;
         // illuminate or blink the required LED's
-            // if correct solid(); else if incorrect, but is somewhere else blink(); else nothing;
+        // if correct solid(); else if incorrect, but is somewhere else blink(); else nothing;
         // prompt the user for their next guess
-            // print_string("\r\n")}
+        // print_string("\r\n")}
         // repeat 10 times or until the user guesses the correct codeword
     }
     
